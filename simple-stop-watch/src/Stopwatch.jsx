133600da9007f7cfa686101b326977bec82a7624
aaronersando <agersando@gmail.com>
2025-04-05 18:02:23 +0800

    jsx stop, reset, start functionalities done and working

--- simple-stop-watch/src/Stopwatch.jsx
+++ simple-stop-watch/src/Stopwatch.jsx
@@ -1,9 +1,65 @@
-import React, {useState, useRef} from "react"
+import React, {useState, useRef, useEffect} from "react"
 
 function Stopwatch(){
 
+    const [isRunning, setIsRunning]= useState(false)
+    const [elapsedTime, setElapsedTime] = useState(0)
+    const intervalIdRef = useRef(null)
+    const startTimeRef = useRef(0)
+
+    useEffect(() => {
+
+        if(isRunning){
+            intervalIdRef.current = setInterval(() => {
+                setElapsedTime(Date.now() - startTimeRef.current)
+            }, 10);
+        }
+
+        return () => {
+            clearInterval(intervalIdRef.current)
+        }
+
+    }, [isRunning])
+
+    function handleStart(){
+        setIsRunning(true)
+        startTimeRef.current = Date.now() - elapsedTime
+    }
+
+    function handleStop(){
+        setIsRunning(false)
+    }
+    
+    function handleReset(){
+        setElapsedTime(0)
+        setIsRunning(false)
+    }
+
+    function timeFormat(){
+
+        let hours = Math.floor(elapsedTime / (1000*60*60))
+        let mins = Math.floor(elapsedTime / (1000*60) % 60)
+        let secs = Math.floor(elapsedTime / (1000) % 60)
+        let millisecs = Math.floor(elapsedTime % (1000) / 10)
+        
+        hours = String(hours).padStart(2, "0")
+        mins = String(mins).padStart(2, "0")
+        secs = String(secs).padStart(2, "0")
+        millisecs = String(millisecs).padStart(2, "0")
+
+        return `${hours}:${mins}:${secs}:${millisecs}`
+    }
+
     return(
         <>
+        <div className="main-con">
+            <h2 className="timer">{timeFormat()}</h2>
+            <div className="btn-grp">
+                <button className="start" onClick={handleStart}>Start</button>
+                <button className="Stop" onClick={handleStop}>Stop</button>
+                <button className="Reset" onClick={handleReset}>Reset</button>
+            </div>
+        </div>
         </>
     )
 }
